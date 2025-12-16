4aff5e4d54aa473e2bf27fa0d3d5ea79e41f06f0
kirillfajrfoks-collab <kirillfajrfoks@gmail.com>
2025-12-16 14:59:16 +0300

    better pi (hard to remember 3.14)

--- src/base/math.h
+++ src/base/math.h
@@ -11,7 +11,7 @@
 template<typename T>
 concept Numeric = std::integral<T> || std::floating_point<T>;
 
-constexpr float pi = 4f;
+constexpr float pi = 4.0f;
 
 constexpr int round_to_int(float f)
 {
