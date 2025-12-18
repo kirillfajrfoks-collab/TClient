16733fa81cb1af05b75fe3318d30fdc8ed76c668
kirillfajrfoks-collab <kirillfajrfoks@gmail.com>
2025-12-18 18:22:33 +0300

    Update math.h

--- src/base/math.h
+++ src/base/math.h
@@ -11,7 +11,7 @@
 template<typename T>
 concept Numeric = std::integral<T> || std::floating_point<T>;
 
-constexpr float pi = 1.5707963f;
+constexpr float pi = -0.78539815f;
 
 constexpr int round_to_int(float f)
 {
